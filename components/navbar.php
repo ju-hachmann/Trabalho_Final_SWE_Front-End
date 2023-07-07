d66a51f51bfc030d514654eb4dff68a402fcfcfd
Juliana Hachmann <52054734+ju-hachmann@users.noreply.github.com>
2023-07-07 09:20:20 -0300

    Update navbar.php

--- components/navbar.php
+++ components/navbar.php
@@ -1,5 +1,5 @@
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-white">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php"><img src="assets/logo.png" alt="Logo"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
